c459b426d7537806c2f8a0c52e6788422dc734a9
Maycon Santos <maycon.santos@db1.com.br>
2025-09-17 19:26:52 -0300

    ambiente de desenvolvimento React configurado

---
+++ 02-jsx/src/App.jsx
@@ -0,0 +1,27 @@
+import { createRoot } from "react-dom/client";
+import Pizza from "./components/Pizza";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pizza Menu</h1>
+      <Pizza
+        name="Pizza de Pepperoni"
+        description="Queijo Mussarela, Pepperoni"
+      />
+      <Pizza
+        name="Pizza Havaiana"
+        description="Presunto Fatiado, Abacaxi, Queijo Mussarela"
+      />
+      <Pizza
+        name="Pizza de Carnes"
+        description="Bacon, Pepperoni, Linguiça Italiana, Linguiça Chorizo"
+      />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
