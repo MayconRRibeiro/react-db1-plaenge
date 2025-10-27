a04a3af02b49d3e726cf0d1282cbbe12f3976e45
Maycon Santos <maycon.santos@db1.com.br>
2025-10-27 18:36:27 -0300

    feat: init 2.0 - navegacao configurada

--- Projetos/devStore/src/app/index.tsx
+++ Projetos/devStore/src/app/index.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
